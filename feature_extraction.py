c9b0f52639e3585764b7d962524e33ff75705345
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-07-22 13:07:57 +0200

    implement lower casing a sentence

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,9 +1,24 @@
 import pandas as pd
+import re
 
-def sentence_statistics(sentence):
+def sentence_statistics(df_sentence):
+    """[summary]
+
+    Args:
+        df_sentence ([dataframe]): Pandas dataframe column with sentences
+    """
 
     # init dataframe
     df_stats = pd.DataFrame()
 
     # count comma
     df_stats["num_comma"] = sentence.str.count(",")
+
+
+def preprocessing(df_sentence):
+
+    # lower case
+    sentence = sentence.str.lower()
+
+    # remove numbers
+

f7c6b4fa28a9820756e667d381411d8997e2736b
Leo Nguyen <Leo.Nguyen@gmx.de>
2021-07-26 17:21:50 +0200

    add imports

--- src/ML_regression.py
+++ src/ML_regression.py
@@ -1,13 +1,14 @@
+from sklearn.linear_model import ElasticNet, Lasso, LinearRegression, Ridge
+from sklearn.ensemble import RandomForestRegressor
 
 
-
-def ML_regression(data, labels, method="linear"):
-    """Various ML regression models.
+def ML_regression(data, labels, method):
+    """Various ML regression models
 
     Args:
         data ([array-like]): training data to train the model on
         labels ([array-like]): correspoding labels for the training data
-        method ([str], optional): regression method to use (options: 'linear', 'lasso', 'ridge', 'elastic-net', 'random-forest'). Defaults to 'linear'.
+        method ([str]): regression method to use (options: 'linear', 'lasso', 'ridge', 'elastic-net', 'random-forest').
 
     Returns:
         reg ([object]): returns a regression model trained on the given data and labels
